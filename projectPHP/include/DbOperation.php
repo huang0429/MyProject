640c0d8100503c8997fc2c8785d6f22f6645fac1
huang0429 <iswork.huang@gmail.com>
2022-10-17 19:40:46 +0800

    更新'

--- projectPHP/include/DbOperation.php
+++ projectPHP/include/DbOperation.php
@@ -4,22 +4,58 @@ class DbOperation
 {
     private $conn;
 
-    //Constructor
     function __construct()
     {
         require_once dirname(__FILE__) . '/Constants.php';
         require_once dirname(__FILE__) . '/DbConnect.php';
-        // 打開資料庫
+        // opening db connection
         $db = new DbConnect();
         $this->conn = $db->connect();
     }
-    
-    //Function to create a new user 註冊
+
+    /*
+     * This method is added
+     * We are taking username and password
+     * and then verifying it from the database
+     * */
+
+    public function userLogin($username, $pass)
+    {
+        $password = md5($pass);
+        $stmt = $this->conn->prepare("SELECT users_ID FROM users WHERE users_name = ? AND users_password = ?");
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $stmt->store_result();
+        return $stmt->num_rows > 0;
+    }
+
+    /*
+     * After the successful login we will call this method
+     * this method will return the user data in an array
+     * */
+
+    public function getUserByUsername($username)
+    {
+        $stmt = $this->conn->prepare("SELECT users_ID, users_name, users_email, users_phone, users_chineseName FROM users WHERE users_name = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->bind_result($id, $name, $email, $phone, $chineseName);
+        $stmt->fetch();
+        $user = array();
+        $user['users_ID'] = $id;
+        $user['users_name'] = $name;
+        $user['users_email'] = $email;
+        $user['users_phone'] = $phone;
+        $user['users_chineseName'] = $chineseName;
+        
+        return $user;
+    }
+
     public function createUser($username, $pass, $email, $name, $phone)
     {
         if (!$this->isUserExist($username, $email, $phone)) {
             $password = md5($pass);
-            $stmt = $this->conn->prepare("INSERT INTO users (username, password, email, name, phone) VALUES (?, ?, ?, ?, ?)");
+            $stmt = $this->conn->prepare("INSERT INTO users (users_name, users_password, users_email, users_chineseName, users_phone) VALUES (?, ?, ?, ?, ?)");
             $stmt->bind_param("sssss", $username, $password, $email, $name, $phone);
             if ($stmt->execute()) {
                 return USER_CREATED;
@@ -30,14 +66,18 @@ class DbOperation
             return USER_ALREADY_EXIST;
         }
     }
+
+
     private function isUserExist($username, $email, $phone)
     {
-        $stmt = $this->conn->prepare("SELECT id FROM users WHERE username = ? OR email = ? OR phone = ?");
+        $stmt = $this->conn->prepare("SELECT users_ID FROM users WHERE users_name = ? OR users_email = ? OR users_phone = ?");
         $stmt->bind_param("sss", $username, $email, $phone);
         $stmt->execute();
         $stmt->store_result();
         return $stmt->num_rows > 0;
     }
+
+
 }
 
 ?>
